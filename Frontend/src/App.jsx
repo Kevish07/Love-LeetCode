67abdb2760f14417acfa3d3a0cf2d34c09a92d93
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-05-25 01:57:03 +0530

    Building login/signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,61 @@
-import './App.css'
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+
+import HomePage from "./page/HomePage.jsx";
+import LoginPage from "./page/LoginPage.jsx";
+import RegisterPage from "./page/RegisterPage.jsx";
+import Layout from "./layout/Layout";
+// import AdminRoute from "./components/AdminRoute";
+// import AddProblem from "./page/AddProblem";
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
 
   return (
     <>
-      <h1>Work in progress</h1>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+        </Route>
+
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/register"
+          element={!authUser ? <RegisterPage /> : <Navigate to={"/"} />}
+        />
+
+        {/* <Route element={<AdminRoute />}>
+          <Route
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to="/" />}
+          />
+        </Route> */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/page/RegisterPage.jsx
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import {
+  Code,
+  Eye,
+  EyeOff,
+  Loader2,
+  Lock,
+  Mail,
+} from "lucide-react"
+
+
+export default function RegisterPage() {
+  return (
+    <div>RegisterPage</div>
+  )
+}
